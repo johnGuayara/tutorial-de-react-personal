b12bc8590f69417590254a069061a912974169c8
john guayara <johnjaider14@gmail.com>
2022-06-13 18:34:59 -0500

    primera version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  // myElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//======================================================================================================
+
+// const myelement = (
+//   <table>
+//     <tr>
+//       <th>Name</th>
+//     </tr>
+//     <tr>
+//       <th>John</th>
+//     </tr>
+//     <tr>
+//       <th>Elsa</th>
+//     </tr>
+//   </table>
+// );
+// ReactDOM.render(myelement,document.getElementById("root"));
+
+//======================================================================================================
+
+
+  // const myElement = (
+  //   <ul>
+  //     <li>Apples</li>
+  //     <li>Bananas</li>
+  //     <li>Cheries</li>
+  //   </ul>
+  // );
+
+//======================================================================================================
+
+
+  // const myElement = (
+//   <div>
+//     <p>I am a paragraph</p>
+//     <p>I am a paragraph too.</p>
+//   </div>
+// );
+
+//======================================================================================================
+
+
+// const myElement = <input type="text" />
+
+//======================================================================================================
+
+// const myElement = <h1 className="myclass">hello world</h1>;
+
+//======================================================================================================
+
+// const x = 5;
+// let text = "Goodbye";
+
+// if(x < 10){
+//   text = "Hello";
+// }
+// const myElement = <h1>{text}</h1>
+
+//======================================================================================================
+
+// const x = 5;
+
+// const myElement = <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>;
